cbcc36795148effbf77806d6019c103969ca28e8
Alain Saab <alainsaab_007@hotmail.com>
2020-05-25 00:14:24 +0300

    End of section 9

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -27,7 +27,7 @@ namespace DatingApp.API.Controllers
         public async Task<IActionResult> GetUsers() {
             var users = await _repo.GetUsers();
             var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
-            return Ok(users);
+            return Ok(usersToReturn);
         }
         [HttpGet("{id}")]
         public async Task<IActionResult> GetUser(int id) {
